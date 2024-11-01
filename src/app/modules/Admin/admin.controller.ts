6a98382f763ff40818b416d731c630239ae3ac26
goni715 <halesgoni715149123@gmail.com>
2024-11-01 23:02:46 +0600

    optimize search & filter

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,7 +2,28 @@ import { Request, Response } from "express";
 import { getAllAdminsService } from "./admin.service";
 
 
+const pick = (obj: Record<string, unknown>, keys: string[]) => {
+  let validFieldsObject: Record<string, unknown> = {}
+  const objKeysArray = Object.keys(obj);
+
+  if(objKeysArray.length > 0){
+    objKeysArray.forEach((key)=> {
+      if(keys.includes(key)){
+        validFieldsObject[key] = obj[key]
+      }
+    })
+  }
+
+  console.log(validFieldsObject);
+  
+
+}
+
+
 const getAllAdmins = async (req: Request, res: Response) => {
+  pick(req.query, ['searchTerm', 'name', 'email', 'contactNumber'])
+
+
     try{
       const result =  await getAllAdminsService(req.query);
       res.status(200).json({
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,32 +3,43 @@ import { Prisma, PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient();
 
 const getAllAdminsService = async (query:any) => {
-
+    const { searchTerm, ...filterData } = query;
     const andConditions: Prisma.AdminWhereInput[] = [];
     //let conditions = {};
+    const serachFields = ['name', 'email', 'contactNumber'];
+    const searchQuery = serachFields.map((item)=>({
+        [item]: {
+            contains: query?.searchTerm,
+            mode: 'insensitive'
+        }
+    }))
+
+
+    // [
+    //     {
+    //         name: {
+    //             contains: query?.searchTerm,
+    //             mode: 'insensitive'
+    //         }
+    //     },
+    //     {
+    //         email: {
+    //             contains: query?.searchTerm,
+    //             mode: 'insensitive'
+    //         }
+    //     },
+    //     {
+    //         contactNumber: {
+    //             contains: query?.searchTerm,
+    //             mode: 'insensitive'
+    //         }
+    //     }
+    // ]
+        
 
     if(query?.searchTerm){
         andConditions.push({
-            OR: [
-                {
-                    name: {
-                        contains: query?.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    email: {
-                        contains: query?.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    contactNumber: {
-                        contains: query?.searchTerm,
-                        mode: 'insensitive'
-                    }
-                }
-            ]
+            OR: searchQuery
         })
 
         // conditions = {
@@ -54,6 +65,33 @@ const getAllAdminsService = async (query:any) => {
         //     ]
         // }
     }
+
+
+    //filter-condition for specific field
+    if(Object.keys(filterData).length > 0){
+        andConditions.push({
+            AND: Object.keys(filterData).map(key=>({
+                [key]: {
+                  equals: filterData[key]
+                }
+            }))
+        })
+    }
+
+    // {
+    //     "AND": [
+    //         {
+    //             "email": "evan@gmail.com"
+    //         },
+    //         {
+    //             "name": "Osman Goni"
+    //         },
+    //         {
+    //             "cotactNumber": "01793837035"
+    //         }
+    //     ]
+    // }
+
 
 
     // whereConditions
