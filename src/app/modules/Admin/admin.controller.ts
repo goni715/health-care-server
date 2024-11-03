cdec3360e9d5ee8bf993c4409f59a046cbf3d138
goni715 <halesgoni715149123@gmail.com>
2024-11-03 16:50:48 +0600

    catchAsync utils

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,66 +1,51 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { deleteAdminService, getAllAdminsService, getSingleAdminService, softDeleteAdminService, updateAdminService } from "./admin.service";
 import { AdminValidFields } from "./admin.constant";
 import pickValidFields from "../../utils/pickValidFields";
 import sendResponse from "../../utils/sendResponse";
+import catchAsync from "../../utils/catchAsync";
 
 
 
-const getAllAdmins = async (req: Request, res: Response, next: NextFunction) => {
 
-    try{
-      const validatedQuery = pickValidFields(req.query, AdminValidFields);
-      const result = await getAllAdminsService(validatedQuery);
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Admins are retrieved successfully",
-        meta: result.meta,
-        data: result.data
-      })
-    }
-    catch(err:any){
-      next(err)
-    }
-  }
+const getAllAdmins = catchAsync(async (req, res) => {
+    const validatedQuery = pickValidFields(req.query, AdminValidFields);
+    const result = await getAllAdminsService(validatedQuery);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admins are retrieved successfully",
+      meta: result.meta,
+      data: result.data
+    })
+})
   
 
 
-  const getSingleAdmin = async (req: Request, res: Response, next:NextFunction) => {
+  const getSingleAdmin = catchAsync(async (req, res) => {
     const { id } = req.params;
+    const result = await getSingleAdminService(id);
 
-    try{
-      const result = await getSingleAdminService(id);
-
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Admin is retrieved successfully",
-        data: result
-      })
-    }
-    catch(err:any){
-     next(err)
-    }
-  }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin is retrieved successfully",
+      data: result,
+    });
+  });
 
 
 
-  const updateAdmin = async (req: Request, res: Response, next:NextFunction) => {
+  const updateAdmin = catchAsync(async (req, res) => {
     const { id } = req.params;
-
-    try {
-      const result = await updateAdminService(id, req.body);
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Admin is updated successfully",
-        data: result
-      })
-    } catch (err: any) {
-      next(err)
-    }
-  };
+    const result = await updateAdminService(id, req.body);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin is updated successfully",
+      data: result,
+    });
+  });
 
 
 
