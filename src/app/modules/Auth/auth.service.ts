893fab965efd37803ef6f064097ee44b3ce2d600
goni715 <halesgoni715149123@gmail.com>
2024-11-03 23:58:47 +0600

    user-login service

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,21 @@
-import { TLoginUser } from "./auth.interface"
+import prisma from "../../shared/prisma"
+import { TLoginUser } from "./auth.interface";
+import bcrypt from 'bcryptjs';
 
 
 const loginUserService = async(payload:TLoginUser) => {
-    return 'Login service'
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: payload.email
+        }
+    })
+
+
+    const isPasswordMatch: boolean = await bcrypt.compare(payload.password, userData.password);
+    console.log(isPasswordMatch);
+
+    return userData;
+
 }
 
 export {
