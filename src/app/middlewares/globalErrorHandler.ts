c9073fe05a9628c9a5b57c756348b18d8c3a8017
goni715 <halesgoni715149123@gmail.com>
2024-11-04 17:03:07 +0600

    ApiError class

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express"
 
 
 const globalErrorHandler = (err:any, req:Request, res: Response, next: NextFunction) => {
-    res.status(500).json({
+    res.status(err.statusCode || 500).json({
       success: false,
       message: err.message || 'Something Weng Wrong',
       error: err
