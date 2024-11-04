b32f97ad1b4277a2a46ef6e3b44432f988f23aeb
goni715 <halesgoni715149123@gmail.com>
2024-11-04 16:45:51 +0600

    AuthMiddleware

---
+++ src/app/middlewares/AuthMiddleware.ts
@@ -0,0 +1,83 @@
+import { NextFunction, Request, Response } from "express";
+import verifyToken from "../utils/verifyToken";
+import config from "../config";
+import { Secret } from "jsonwebtoken";
+import prisma from "../shared/prisma";
+
+type TUserRole = "admin" | "super_admin" | "doctor" | "patient";
+
+const AuthMiddleware = (...roles: TUserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) : Promise<any> => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "You are not authorized",
+          error: "jwt token must be provided",
+        });
+      }
+
+      //token-verify
+      const { email, role } = verifyToken(
+        token,
+        config.jwt_access_secret as Secret
+      );
+
+
+      //check if role is matching
+      if (roles.length > 0 && !roles.includes(role)) {
+        return res.status(401).json({
+             success: false,
+             message: "You are not authorized",
+             error: `User role is ${role}`
+        });
+      }
+
+      const userExist = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      //check if user is not exist
+      if (!userExist) {
+        return res.status(401).json({
+             success: false,
+             message: "You are not authorized",
+             error: "This user is not existed"
+        });
+      }
+
+      //check if the user is deleted
+      if (userExist.isDeleted) {
+        return res.status(401).json({
+             success: false,
+             message: "You are not authorized",
+             error: 'This user is deleted'
+         });
+      }
+
+      //check if the user is blocked
+      const blockStatus = userExist.status;
+      if (blockStatus === "blocked") {
+        return res.status(401).json({
+            success: false,
+            message: "You are not authorized",
+            error: 'This user is blocked'
+        });
+      }
+
+      next()
+
+    } catch (err: any) {
+      res.status(401).json({
+        success: false,
+        message: "You are not authorized",
+        error: err.message,
+      });
+    }
+  };
+};
+
+export default AuthMiddleware;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { userController } from './user.controller';
+import AuthMiddleware from '../../middlewares/AuthMiddleware';
 
 const router = express.Router();
 
 
 
-router.post('/create-admin', userController.createAdmin)
+router.post('/create-admin', AuthMiddleware('admin'), userController.createAdmin)
 
 
 
