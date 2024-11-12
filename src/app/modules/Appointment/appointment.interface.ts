3a2c8489287c322baa3f350ae0f2eba4ec5e0986
goni715 <halesgoni715149123@gmail.com>
2024-11-12 16:32:45 +0600

    create-appointment

---
+++ src/app/modules/Appointment/appointment.interface.ts
@@ -0,0 +1,7 @@
+
+export type TAppointment = {
+    patientId?: string;
+    doctorId: string,
+    scheduleId: string,
+    videoCallingId: string;
+}
--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -1,7 +1,109 @@
+import ApiError from "../../errors/ApiError";
+import prisma from "../../shared/prisma";
+import { TAppointment } from "./appointment.interface";
 
 
-const createAppointmentService = async (email: string, payload:any) => {
-    return payload;
+const createAppointmentService = async (email: string, payload:TAppointment) => {
+  const patientExist = await prisma.patient.findUnique({
+    where: {
+      email,
+    }
+  });
+
+  //check if patient does not exist
+  if (!patientExist) {
+    throw new ApiError(404, "Patient does not exist");
+  }
+
+  //set patientId
+  payload.patientId= patientExist.id;
+
+
+
+  const doctorExist = await prisma.doctor.findUnique({
+    where: {
+      id: payload.doctorId
+    }
+  });
+
+  //check if doctor does not exist
+  if (!doctorExist) {
+    throw new ApiError(404, "doctorId does not exist");
+  }
+
+
+  const scheduleExist = await prisma.schedule.findUnique({
+    where: {
+      id: payload.scheduleId
+    }
+  });
+
+  //check if schedule does not exist
+  if (!scheduleExist) {
+    throw new ApiError(404, "scheduleId does not exist");
+  }
+
+
+
+  const doctorScheduleExist = await prisma.doctorSchedules.findUnique({
+    where: {
+      doctorId_scheduleId: {
+        doctorId: payload.doctorId,
+        scheduleId: payload.scheduleId,
+      }
+    }
+  });
+
+  //check if doctorSchedule does not Exist
+  if(!doctorScheduleExist){
+    throw new ApiError(404, "This scheduleId does not match with doctorId");
+  }
+
+
+  const isBookedSchedule = await prisma.doctorSchedules.findUnique({
+    where: {
+      doctorId_scheduleId: {
+        doctorId: payload.doctorId,
+        scheduleId: payload.scheduleId
+      },
+      isBooked: true
+    }
+   })
+
+
+   if(isBookedSchedule){
+    throw new ApiError(403, `This doctorSchedule is already booked`);
+   }
+
+
+
+
+  const result = await prisma.$transaction(async (tx) => {
+    //query-01 createAppointment
+    const createdAppointmentData = await tx.appointment.create({
+        data: payload
+    })
+
+    //query-02 update doctorSchedule
+    await tx.doctorSchedules.update({
+        where: {
+            doctorId_scheduleId: {
+              doctorId: payload.doctorId,
+              scheduleId: payload.scheduleId,
+            }
+          },
+        data: {
+            isBooked: true,
+            appointmentId: createdAppointmentData.id
+        }
+    })
+
+    return createdAppointmentData
+
+  })
+
+
+  return result;
 }
 
 
