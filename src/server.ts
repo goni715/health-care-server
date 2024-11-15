fb37a8b985754f9a3fc23b082f168f414cc2783f
goni715 <halesgoni715149123@gmail.com>
2024-11-15 11:24:13 +0600

    uncaughtException & unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,30 @@ import app from "./app";
 const port = 5000;
 
 async function main() {
+
   const server: Server = app.listen(port, () => {
     console.log(`server running at ${port} port`);
   });
+
+  const exitHandler = () => {
+    if(server){
+      server.close(()=> {
+        console.log('Server closed');
+      })
+    }
+    process.exit(1)
+  }
+
+
+  process.on('uncaughtException', (error)=> {
+    console.log(error);
+    exitHandler();
+  })
+
+  process.on('unhandledRejection', (error)=> {
+    console.log(error);
+    exitHandler();
+  })
 }
 
 main();
